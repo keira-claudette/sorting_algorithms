bd9ddbbba937d43606eac06ec0a880b4d817ec1f
keira-claudette <claudette.keira@gmail.com>
2021-06-16 18:29:42 +0000

    updated header file

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void swap(listint_t *node1, listint_t *node2, listint_t **head);
+void insertion_sort_list(listint_t **list);
+int partition(int *arr, int start_idx, int last_idx, int size);
+void quick(int *arr, int start_idx, int last_idx, int size);
 #endif
